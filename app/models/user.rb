6f03061a50d380aabafbbe8fcd618aa888493490
Syosen33 <syosen33@gmail.com>
2024-07-02 15:56:12 +0900

    修正２回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,26 +6,12 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナで入力してください' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナで入力してください' }
   validates :birthday, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }, confirmation: true
- # パスワードとパスワード確認の値が一致していることを確認するカスタムバリデーション
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
+   # パスワードとパスワード確認の値が一致していることを確認するカスタムバリデーション
 
-  def password_match
-    if password != password_confirmation
-      errors.add(:password_confirmation, "doesn't match Password")
-    end
-  end
 
   # devise のモジュールを含む
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def custom_validation
-  if some_condition
-    errors[:base] << "Custom error message 1"
-  end
-
-  if another_condition
-    errors[:base] << "Custom error message 2"
-  end
-end
 end
