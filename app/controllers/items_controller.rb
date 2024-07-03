ded1e9fd6b331324557371475d6e9b65fb40f2bc
Syosen33 <syosen33@gmail.com>
2024-07-03 17:47:20 +0900

    ActiveHashカテゴリ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,27 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @items = Item.order("created_at DESC")
+  end
 
   def new
     @item = Item.new
   end
 
-  def index
-    @items = Item.all
-  end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      # バリデーションエラーメッセージを表示するために @item.errors を使います
+      render :new
+    end
   end
   
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :item_image, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, :price)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  # 空の投稿を保存できないようにする
+  validates :title, :text, presence: true
+
+  # 商品名と説明のバリデーション
+  validates :product_name, presence: true
+  validates :description, presence: true
+
+  # カテゴリーの選択が「---」の時は保存できないようにする
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :condition_id, presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+
+  # 価格のバリデーション
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # 追加のバリデーション例:
+  # validates :product_name, length: { maximum: 100 }
+  # validates :description, length: { maximum: 1000 }
 end
