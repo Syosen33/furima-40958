4c5a17d37d5c1ced33aab2f432cf54da9b48e7bd
Syosen33 <syosen33@gmail.com>
2024-07-12 05:54:00 +0900

    エラーメッセージを購入画面に表示

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,9 +2,11 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number,:token
 
-  validates :postal_code, :prefecture_id, :city, :street_address, :phone_number, presence: true
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'must be in the format XXX-XXXX' }
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'must be 10 or 11 digits' }
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁-4桁」の形式で入力してください" }
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :street_address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数字で入力してください" }
 
   def save
     return false unless valid?
