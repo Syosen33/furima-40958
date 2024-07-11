7ab4c948bb6692722d70eeafbc9361b096138fa7
Syosen33 <syosen33@gmail.com>
2024-07-12 06:30:12 +0900

    エラーハンドリング完成

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,12 +1,15 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number,:token
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :token
 
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁-4桁」の形式で入力してください" }
   validates :prefecture_id, presence: true
   validates :city, presence: true
   validates :street_address, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数字で入力してください" }
+
+   validates :token, presence: true
+  
 
   def save
     return false unless valid?
